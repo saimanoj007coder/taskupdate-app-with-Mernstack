cf937d2f6ea06ea5e8d7e7dfd507ab1b7aa0885c
saimanoj007coder <jackmanoj8686@gmail.com>
2023-03-26 19:33:05 +0530

    second commit

--- server/models/db.js
+++ server/models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-module.exports = mongoose.connect('mongodb://127.0.0.1:27017/todolist',{
+module.exports = mongoose.connect('mongodb+srv://jackmanoj8686:100171007@atlascluster.bshuteh.mongodb.net/todolist',{
     useUnifiedTopology : true,
     useNewUrlParser : true,
     useFindAndModify: false 
